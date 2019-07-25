44a9ab2238234d86085b9b904f94b7ff4e644ef8
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2019-07-24 17:37:49 -0700

    compras v1.0.1

--- ajaxsubmit-compra.php
+++ ajaxsubmit-compra.php
@@ -12,7 +12,6 @@ $compra_preciounidad2=$_POST['compra_preciounidad1'];
 $compra_fechacompra2=$_POST['compra_fechacompra1'];
 $compra_lugarcompra2=$_POST['compra_lugarcompra1'];
 
-
 //Insert query
 $controlPollo->query("INSERT INTO `compras`(`id_compra`, `cantidad_compra`, `preciounidad_compra`, `fecha_compra`, `lugar_compra`, `name_user`)
       VALUES (null,'".$compra_cantidad2."','".$compra_preciounidad2."','".$compra_fechacompra2."','".$compra_lugarcompra2."','".$name_user2."')");
--- pruebaadd.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "probando git add";
- ?>
